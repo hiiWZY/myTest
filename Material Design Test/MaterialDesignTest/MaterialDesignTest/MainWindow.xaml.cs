77e7a4d452cd42f0c161ddf9e78127ea70257ae8
hiiWZY <hiiwzy@163.com>
2018-01-10 23:39:30 +0800

    dynamic add control
    dynamic add control
    try binding property  to database

--- Material Design Test/MaterialDesignTest/MaterialDesignTest/MainWindow.xaml.cs
+++ Material Design Test/MaterialDesignTest/MaterialDesignTest/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaterialDesignTest.LeakTestDataSetTableAdapters;
 
 namespace MaterialDesignTest
 {
@@ -20,9 +21,35 @@ namespace MaterialDesignTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        LeakTestDataSet leakTestDataSet = new LeakTestDataSet();
+        UsersTableAdapter usersTableAdapter = new UsersTableAdapter();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            LoadUserCards();
+        }
+
+        private void LoadUserCards()
+        {
+            //usersTableAdapter.GetUsersScalar();
+            MainCanvas.Children.Clear();
+            for (int i = 0; i < usersTableAdapter.GetUsersScalar(); i++)
+            {
+                MainCanvas.Children.Add(new controlUserCards());
+            }
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadUserCards();
         }
     }
 }
